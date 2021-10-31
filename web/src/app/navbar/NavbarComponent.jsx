f21b8af33fde5a312ad97feab9b9d7a5de758236
Vaibhav Negi <10vaibhavsinghnegi@gmail.com>
2021-10-31 21:07:04 +0530

    refactor(web): Navbar styling and text.

--- web/src/app/navbar/NavbarComponent.jsx
+++ web/src/app/navbar/NavbarComponent.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { ScaleIcon } from "../common/icons";
+
 export default function NavbarComponent() {
   return (
     <nav className="p-5 shadow-md">
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" className="flex hover:text-blue-600">
+            <ScaleIcon className="w-6 h-6" />
+            &nbsp;
+            <span className="font-medium">Opinion</span>
+          </Link>
         </li>
       </ul>
     </nav>
