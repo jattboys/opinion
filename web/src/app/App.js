5907ba9d681c621d5c7524b734a58299290c3943
Vaibhav Negi <10vaibhavsinghnegi@gmail.com>
2021-10-31 20:34:15 +0530

    refactor(web): Move navbar to a different dir.

--- web/src/app/App.js
+++ web/src/app/App.js
@@ -1,24 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import HomeContainer from "./home/HomeContainer";
 import PollContainer from "./poll/PollContainer";
+import NavbarComponent from "./navbar/NavbarComponent";
 
 export default function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/create">Create</Link>
-            </li>
-          </ul>
-        </nav>
-
+        <NavbarComponent />
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
---
+++ web/src/app/navbar/NavbarComponent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+export default function NavbarComponent() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/create">Create</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
