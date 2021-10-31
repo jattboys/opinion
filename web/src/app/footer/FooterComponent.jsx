823a142c3114278ad4914929d7c53e850bf4e801
Vaibhav Negi <10vaibhavsinghnegi@gmail.com>
2021-10-31 21:07:59 +0530

    refactor(web): Make footer icon bigger.

--- web/src/app/footer/FooterComponent.jsx
+++ web/src/app/footer/FooterComponent.jsx
@@ -7,8 +7,8 @@ export default function FooterComponent() {
   return (
     <section className="fixed bottom-5 right-5">
       <Link to="/create">
-        <p className="flex p-2 text-white bg-blue-600 rounded-full hover:shadow-lg">
-          <PlusIcon className="w-6 h-6 " />
+        <p className="flex items-center justify-center p-4 text-white bg-blue-600 rounded-full hover:shadow-lg">
+          <PlusIcon className="w-6 h-6" />
         </p>
       </Link>
     </section>
