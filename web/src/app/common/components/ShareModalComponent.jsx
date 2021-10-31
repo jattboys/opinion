482ac07652ab3e7f19c4d5bcee7e7f1ee29f2485
Vaibhav Negi <10vaibhavsinghnegi@gmail.com>
2021-10-31 23:05:07 +0530

    feat(web): Add qr code in share modal.

--- web/src/app/common/components/ShareModalComponent.jsx
+++ web/src/app/common/components/ShareModalComponent.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import QRCode from "react-qr-code";
 
 import { SolidPlusIcon } from "../icons";
 
@@ -7,7 +8,7 @@ export default function ShareModalComponent({ showModal, setShowModal }) {
     showModal && (
       <section className="fixed inset-0 w-full h-full bg-gray-400 bg-opacity-30">
         <div className="flex items-center justify-center w-full h-full">
-          <div className="p-12 bg-white rounded-lg opacity-100 w-96 h-80">
+          <div className="flex flex-col p-12 space-y-10 bg-white rounded-lg opacity-100 w-96">
             {/* Header */}
             <div className="flex justify-between">
               <span className="text-xl font-medium">Share this poll</span>
@@ -17,6 +18,10 @@ export default function ShareModalComponent({ showModal, setShowModal }) {
                   setShowModal(false);
                 }}
               />
+            </div>
+
+            <div className="flex justify-center">
+              <QRCode value={window.location.href} />
             </div>
           </div>
         </div>
