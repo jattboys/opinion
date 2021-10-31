5f436bdbe88beef8219d1f234a58c6378de73c98
Shouvik Mitra <shouvik@navigus.in>
2021-10-31 20:26:14 +0530

    feat(web): create new opinion

--- web/src/app/poll/PollComponent.jsx
+++ web/src/app/poll/PollComponent.jsx
@@ -1,5 +1,38 @@
 import React from "react";
 
-export default function PollComponent() {
-  return <div>Polls Component</div>;
+export default function PollComponent({ handleSubmit }) {
+  return (
+    <div className="flex flex-row justify-center items-center w-screen mt-12">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (e.target.elements.title.value.length > 0) {
+            handleSubmit(e.target.elements.title.value);
+          } else {
+            alert("Please enter what you want to ask!");
+          }
+        }}
+        className="border border-gray-800 rounded-md shadow-lg flex flex-col p-4"
+        style={{ maxWidth: "40rem", width: "100%" }}
+      >
+        <label className="text-left font-bold text-2xl pb-4" htmlFor="title">
+          What do you feel confused about?
+        </label>
+        <input
+          placeholder="Type something which needs other opinions here"
+          type="text"
+          name="title"
+          id="title"
+          className="rounded-sm"
+          autoFocus
+        />
+        <button
+          type="submit"
+          className="p-2 mt-10 bg-gray-700 rounded-sm text-white hover:bg-blue-600  hover:shadow-md"
+        >
+          Ask
+        </button>
+      </form>
+    </div>
+  );
 }
